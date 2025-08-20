b0aed10c8a7c0a58abbaf3666b872482f2318f1a
unknown <junaidmollah17@gmail.com>
2025-08-20 21:11:20 +0530

    Fixed some backend related issue

--- frontend/client/src/contracts/loan-abi.js
+++ frontend/client/src/contracts/loan-abi.js
@@ -621,5 +621,5 @@ export const LOAN_CONTRACT_ABI = [
 ];
 
 // ✅ IMPORTANT: Replace with your deployed contract address after deployment
-export const LOAN_CONTRACT_ADDRESS = "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0";
+export const LOAN_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
