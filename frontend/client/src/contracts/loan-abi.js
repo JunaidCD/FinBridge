117eb2f756ad9d589b463816d4d1fe31e158ce5d
JunaidCD <junaidmollah17@gmail.com>
2026-02-08 22:22:25 +0530

    Changed all ts and .tsx file into js and .jsx file

--- frontend/client/src/contracts/loan-abi.js
+++ frontend/client/src/contracts/loan-abi.js
@@ -621,4 +621,4 @@ export const LOAN_CONTRACT_ABI = [
 ];
 
 // ✅ IMPORTANT: Replace with your deployed contract address after deployment
-export const LOAN_CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const LOAN_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
